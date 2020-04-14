ddbc4c182798dab437997c42ccdc95a815a31ece
SOLOMON YISMAW <fantahunsolomon@gmail.com>
2020-04-14 06:59:16 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -6,13 +6,13 @@
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
-introduction_with_language
+
 
 
 def introduction_with_language_optional(name, language = Ruby)
   puts "Hi, my name is #{name} and I am learning to program in #{language}"
 end
-introduction_with_language_optional
+
 
 
 
